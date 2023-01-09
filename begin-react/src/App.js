720a7a3ff0375dfff4f4871b90bea2d900fa9a12
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-01-09 13:22:45 +0900

    feat : 1장 진행중

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,6 +1,7 @@
 import React, { useRef,useMemo, useCallback, useReducer } from 'react';
 import UserList from './UserList'
 import CreateUser from './CreateUser';
+import useInputs from './useInputs';
 
 function countActiveUsers(users){
   console.log('활성 사용자 수를 세는중...');
@@ -36,14 +37,6 @@ const initialState = {//초기상태
 
 function reducer(state, action){//새로운 상태를 만드는 로직이다.
   switch (action.type) {//action의 타입을 확인하여서 실행
-    case 'CHAGNE_INPUT':
-      return{
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
     case 'CREATE_USER':
       return{
         inputs : initialState.inputs,
@@ -67,17 +60,22 @@ function reducer(state, action){//새로운 상태를 만드는 로직이다.
 }
 
 function App(){
+  const [{username,email},onChange,reset] = useInputs({
+    username: '',
+    email:''
+  });
   const [state, dispatch] = useReducer(reducer, initialState);
   //useReducer의 활용법으로
   //state = 우리가 앞으로 컴포넌트에서 사용 할 수 있는 상태를 가르키게 된다.
   //dispath는 액션을 발생 시키는 함수 => dispatch{{type: 'typename'}}
   //useReducer는 reducer와 초기상태인 initialState를 매개변수로 받는다 
-
   const nextId = useRef(4);
-  const { users } = state;
-  const {username,email} = state.inputs;
 
-  const onChange = useCallback(e => {//useCallback을 활용해서 특정 함수를 새로 만들지 않고 재사용
+  const { users } = state;
+
+  //const {username,email} = state.inputs;
+
+  /*const onChange = useCallback(e => {//useCallback을 활용해서 특정 함수를 새로 만들지 않고 재사용
     //useCallback의 첫번째 매개변수로 어떻게 연산을 정의할지 정의하는 함수
     const {name, value}  = e.target;
     dispatch({
@@ -87,7 +85,7 @@ function App(){
     });
   },[]);
   //useCallback의 두번째 매개변수로 배열 값이 바뀌게되면 우리가 등록한 함수를 호출해서 연산을 해주고
-  //배열의 값이 바뀌지 않으면 이전에 연산한 값을 재사용하게 된다.
+  //배열의 값이 바뀌지 않으면 이전에 연산한 값을 재사용하게 된다.*/
 
   const onCreate = useCallback(() => {
     dispatch({
@@ -98,8 +96,9 @@ function App(){
         email
       }
     });
+    reset();
     nextId.current += 1;
-  },[username,email])
+  },[username,email,reset])
 
   const onToggle = useCallback(id => {
     dispatch({
